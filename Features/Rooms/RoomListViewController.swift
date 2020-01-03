76a7a3466da28f7496a8683452d80bf46a6570af
Rufus <Rufusmall@hotmail.com>
2020-01-03 10:46:29 +0000

    Split the room feature into its own module.

--- Features/Rooms/RoomListViewController.swift
+++ Features/Rooms/RoomListViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Common
 
-class RoomListViewController: UIViewController {
+public class RoomListViewController: UIViewController {
     var viewModel: RoomListViewModel!
     var roomService: RoomServiceProtocol!
     
@@ -29,7 +29,7 @@ class RoomListViewController: UIViewController {
         }
     }
     
-    init(roomService: RoomServiceProtocol) {
+    public init(roomService: RoomServiceProtocol) {
         super.init(nibName: nil, bundle: nil)
         self.tabBarItem = UITabBarItem(title: "room", systemName: "studentdesk")
         self.roomService = roomService
@@ -60,7 +60,7 @@ class RoomListViewController: UIViewController {
         return tableView
     }()
     
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self
@@ -74,19 +74,18 @@ class RoomListViewController: UIViewController {
         viewModel.start()
     }
     
-    @objc
-      func refreshControlChanged(sender: Any?) {
+    @objc func refreshControlChanged(sender: Any?) {
           viewModel.refresh()
       }
 }
 
 extension RoomListViewController: UITableViewDataSource {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel.state.rooms.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let roomVM = viewModel.state.rooms[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: RoomCell.cellID, for: indexPath) as! RoomCell
         cell.viewModel = roomVM
---
+++ Features/Rooms/Rooms/Rooms.h
@@ -0,0 +1,19 @@
+//
+//  Rooms.h
+//  Rooms
+//
+//  Created by Rufus on 03/01/2020.
+//  Copyright © 2020 Rufus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Rooms.
+FOUNDATION_EXPORT double RoomsVersionNumber;
+
+//! Project version string for Rooms.
+FOUNDATION_EXPORT const unsigned char RoomsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Rooms/PublicHeader.h>
+
+
